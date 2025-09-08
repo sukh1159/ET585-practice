970df8cb0dace2e4c780a4274002c908f457993f
sukh1159 <sukhjot.randhawa64@student.qcc.cuny.edu>
2025-09-08 05:38:13 +0000

    Java practice ex 3

---
+++ L1_ex/test_2.java
@@ -0,0 +1,23 @@
+/*
+ 3. Print following text to the console, please use ‘\t’ and ‘\n’.
+Item price
+Apple 1.75
+Orange 3.50
+Banana 2.25
+Total 9.00
+
+ */
+
+ public class test_2{
+    public static void main(String[] args){
+        System.out.println("One Line Code;\n");
+        System.out.println("Item\tprince\nApple\t1.75\nOrange\t3.50\nBanana\t2.25\nTotal\t9.00\n");
+
+        System.out.println("Multiple lines code;\n");
+        System.out.println("Item\tprince");
+        System.out.println("Apple\t1.75");
+        System.out.println("Orange\t3.50");
+        System.out.println("Banana\t2.25");
+        System.out.println("Total\t9.00");
+    }
+ }
