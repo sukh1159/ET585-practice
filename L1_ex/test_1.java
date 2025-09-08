ccd827a1c05173c00759f0277cf011acebf16c81
sukh1159 <sukhjot.randhawa64@student.qcc.cuny.edu>
2025-09-08 05:15:32 +0000

    first java code

---
+++ L1_ex/test_1.java
@@ -0,0 +1,13 @@
+/*
+1. Write a script to Print the message “I am new to Java program!”.
+2. Write a program that prints your own name, “Hello, xxx”.
+
+ */
+
+
+public class test_1 {
+    public static void main(String[] args){
+        System.out.println("I am new to Java Program!");
+        System.out.println("Hello,Sukh");
+    }
+}
