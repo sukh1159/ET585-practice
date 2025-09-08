19bd36cc521d9922e74e861822e095e3019db4bd
sukh1159 <sukhjot.randhawa64@student.qcc.cuny.edu>
2025-09-08 05:45:46 +0000

    java practice ex4

---
+++ L1_ex/test_3.java
@@ -0,0 +1,18 @@
+/*
+ 4. Print following text to the console.
+Name ID
+Tom Brown 15267789
+My name is very long 12345678
+David 99999999
+ */
+
+
+public class test_3 {
+    public static void main (String[] args){
+        System.out.printf("Name\t\t\tID\n");
+        System.out.printf("Tom Brown\t\t15267789\n");
+        System.out.printf("My name is very long\t12345678\n");
+        System.out.printf("David\t\t\t99999999\n");
+    }
+    
+}
